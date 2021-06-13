47b0bf9e8b577d3353ec28018994abaf48b849a2
LambdaRamen02 <58314484+LambdaRamen02@users.noreply.github.com>
2021-06-13 01:46:32 -0700

    Initial Commit

---
+++ main.js
@@ -0,0 +1,12 @@
+const Discord = require('discord.js');
+
+const client = new Discord.Client();
+
+client.once('ready', () => {
+    console.log('VikingBot is online!');
+});
+
+
+//END OF FILE
+client.login('ODUzNTUyMzQ4MDA0ODc2MzA4.YMXCkg.av7Zf0IRYkW_3S9WT9zdSEh6U5E');
+
