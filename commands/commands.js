9d0bc12e194367fb709dbdafd634211f8d0b5c33
LambdaRamen02 <58314484+LambdaRamen02@users.noreply.github.com>
2021-07-30 16:55:59 -0700

    Attempting to read from a  .txt file for commands

--- commands/commands.js
+++ commands/commands.js
@@ -1,8 +1,15 @@
+const fs = require('fs');
+
 module.exports = {
     name: 'commands',
     description: 'prints a list of available commands for VikingBot',
     execute(message, args) {
         // sends message to channel with a list of all commands and their functions
-        message.channel.send('cheese\ngaming');
+        let reader = new FileReader();
+        let animal = "cheese";
+        // message.channel.send("**COMMANDS:**\n!ping : responds with \"pong\" \n!daddy or !daddywatts : responds with daddywatts of course");
+        message.channel.send(reader.readAsText("commands.txt"));
+        message.channel.send(animal);
     }
+
 }
