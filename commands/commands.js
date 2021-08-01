ab6a5c619c4b2fb6abed4130d6ad3fb460c216ca
LambdaRamen02 <58314484+LambdaRamen02@users.noreply.github.com>
2021-07-31 22:10:38 -0700

    Got the bot to read the .txt file W

--- commands/commands.js
+++ commands/commands.js
@@ -1,15 +1,18 @@
+// Importing node js fs module
 const fs = require('fs');
 
 module.exports = {
     name: 'commands',
     description: 'prints a list of available commands for VikingBot',
     execute(message, args) {
-        // sends message to channel with a list of all commands and their functions
-        let reader = new FileReader();
-        let animal = "cheese";
-        // message.channel.send("**COMMANDS:**\n!ping : responds with \"pong\" \n!daddy or !daddywatts : responds with daddywatts of course");
-        message.channel.send(reader.readAsText("commands.txt"));
-        message.channel.send(animal);
+        
+        // Prints the commands.txt file to the chat, and prints any errors to the node js console
+        try {
+            let content = fs.readFileSync('/Users/codyh/Desktop/Summer2021/discord-bot/commands/commands.txt', 'utf8');
+            message.channel.send(content);
+        } catch(err) {
+            console.error(err);
+        }
     }
 
 }
