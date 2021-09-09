bee4a0174f09bd5694d32ef09affbb54e70a8411
coheale <58314484+coheale@users.noreply.github.com>
2021-09-09 14:02:33 -0700

    Update shaggy.js

--- commands/shaggy.js
+++ commands/shaggy.js
@@ -1,6 +1,6 @@
 module.exports = {
     name: 'shaggy',
-    description: "do the swag",
+    description: "Sends a funny photo of Shaggy",
     execute(message, args) {
         // sends message to channel consisting of provided provided link
         message.channel.send('https://pbs.twimg.com/media/DNfcOdpWAAYhDoo.jpg');
